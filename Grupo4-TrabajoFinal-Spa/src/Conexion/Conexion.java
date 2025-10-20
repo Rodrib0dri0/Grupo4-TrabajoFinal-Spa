f0f190991fd9ee4fca32f6b06737724218376d7a
Rodrigo Muñoz Villegas <rodrigomunozv4@gmail.com>
2025-10-20 17:00:48 -0300

    Clase conexion conectada a la base de datos

--- Grupo4-TrabajoFinal-Spa/src/Conexion/Conexion.java
+++ Grupo4-TrabajoFinal-Spa/src/Conexion/Conexion.java
@@ -1,13 +1,44 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Conexion;
 
-/**
- *
- * @author mikur
- */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
 public class Conexion {
+    
+    private String url = "jdbc:mariadb://localhost:3306/grupo4-trabajofinal-spa";
+    private String usuario = "root";
+    private String contra = "";
+            
+            
+    private static Connection conexion = null;
+
+    public Conexion(String url, String usuario, String contra) {
+        this.url=url;
+        this.usuario=usuario;
+        this.contra=contra;
+    }
+    
+    
+
+    public Connection buscarconexion() {
+        if (conexion == null){
+            
+        try {
+
+            Class.forName("org.mariadb.jdbc.Driver");
+            conexion = DriverManager.getConnection(url, usuario, contra);
+
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error de conexion" + ex);
+
+        } catch (ClassNotFoundException ex) {
+            JOptionPane.showMessageDialog(null, "Error al cargar drive" + ex);
+        }
+        }
+        return conexion;
+    }
     
 }
