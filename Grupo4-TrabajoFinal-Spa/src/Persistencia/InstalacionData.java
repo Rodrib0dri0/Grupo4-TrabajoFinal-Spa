239d0031eddc129b4f599dd1a476bfaa2164f6e0
Rodrigo Muñoz Villegas <rodrigomunozv4@gmail.com>
2025-10-21 02:04:24 -0300

    ns

--- Grupo4-TrabajoFinal-Spa/src/Persistencia/InstalacionData.java
+++ Grupo4-TrabajoFinal-Spa/src/Persistencia/InstalacionData.java
@@ -1,17 +1,59 @@
 package Persistencia;
 
 import Modelo.Conexion;
+import Modelo.Instalacion;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.swing.JOptionPane;
 
 public class InstalacionData {
-        private Connection con;
-    
+
+    private Connection con;
+
     public InstalacionData() {
         Conexion conexion = new Conexion("jdbc:mariadb://localhost:3306/grupo4-trabajofinal-spa", "root", "");
         con = conexion.buscarconexion();
     }
+
+    public void insertarInstalacion(Instalacion inst) {
+        try {
+            String sql = "INSERT INTO instalacion(nombre, uso, precio30m, estado) VALUES (?,?,?,?)";
+            PreparedStatement ps = con.prepareStatement(sql);
+
+            ps.setString(1, inst.getNombre());
+            ps.setString(2, inst.getDetalleDeUso());
+            ps.setDouble(3, inst.getPrecio30m());
+            ps.setBoolean(4, inst.isEstado());
+
+            int registro = ps.executeUpdate();
+
+            if (registro > 0) {
+                JOptionPane.showMessageDialog(null, "Instalación guardada correctamente!");
+            }
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al guardar.");
+        }
+    }
     
-    public void insertarInsatalacion(){
+    public void eliminarInstalacion(int id){
+        try {
+            String sql = "DELETE FROM instalacion WHERE ?";
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setInt(1, id);
+            
+            int registro = ps.executeUpdate();
+            
+            if(registro > 0){
+                JOptionPane.showMessageDialog(null, "Instalación eliminada correctamente!");
+            }
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al eliminar.");
+        }
+        
         
     }
 }
